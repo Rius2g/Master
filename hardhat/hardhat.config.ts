f2630c0659d0dafc8f1cfc154fcb90bf714bf48b
Rius2g <marius.mollerhansen@hotmail.com>
2025-02-25 15:56:55 +0100

    skeleton for the 2 demos of the program/flow

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28", // For Lock.sol
+      },
+      {
+        version: "0.8.19", // For Master.sol
+      },
+    ],
+  },
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 43113,
+    },
+  },
 };
 
 export default config;
